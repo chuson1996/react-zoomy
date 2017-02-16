7250003dba22afffd116f92590e71c3376e3554e
Chu Hoang Son <chuson1996@gmail.com>
2017-02-16 10:07:09 +0100

    Updates

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactZoomImage from './lib/react-zoomy.js';
+import ReactZoomy from './lib/react-zoomy.js';
 
 ReactDOM.render(
-  <ReactZoomImage
+  <ReactZoomy
     imageUrl={'https://images.unsplash.com/photo-1441986300917-64674bd600d8?dpr=2&auto=format&fit=crop&w=767&h=512&q=80&cs=tinysrgb&crop='}
     renderThumbnail={({ showImage }) => <button onClick={showImage}>Show Image</button>}
   />,
